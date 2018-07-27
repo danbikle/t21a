ab03b3db9e3a62cd6f2e72f41d73e7dd5916d6f8
Dan Bikle <danbikle@outlook.com>
2018-07-27 14:13:01 -0700

    more

--- script/genf.py
+++ script/genf.py
@@ -8,3 +8,17 @@ cd ~/t21a/script/
 ~/anaconda3/bin/python genf.py ${HOME}/req/csv/history/TLT.csv
 """
 
+import datetime
+import os
+import re
+import requests
+import sys
+import time
+import pdb
+
+if (len(sys.argv) != 2):
+  print('You should give the name of a CSV-file full of dates and prices.')
+  print('Demo:')
+  print('~/anaconda3/bin/python genf.py ${HOME}/req/csv/history/TLT.csv')
+  sys.exit(1)
+    
