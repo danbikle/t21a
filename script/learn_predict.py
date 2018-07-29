53d3f3aeae5ebc3a6f789aca81ac142a685ff3e6
Dan Bikle <danbikle@outlook.com>
2018-07-28 22:33:40 -0700

    more

--- script/learn_predict.py
+++ script/learn_predict.py
@@ -28,4 +28,25 @@ if (len(sys.argv) != 2):
 # I should get the path of the CSV-file:
 file_s = sys.argv[1]
 
+train_df = pd.read_csv(file_s)
+
+train_a = np.array(train_df)[9:]
+
+# I should declare some integers to help me navigate the Arrays.
+cdate_i    = 0
+cp_i       = 1
+pctlead_i  = 2
+pctlag1_i  = 3
+
+x_train_a = train_a[:,pctlag1_i:]
+y_train_a = train_a[:,pctlead_i]
+# I should learn from x_train_a,y_train_a:
+from sklearn import linear_model
+
+linr_mod = linear_model.LinearRegression()
+linr_mod.fit(x_train_a, y_train_a)
+
+# Now that I have learned, I should predict:
+
+  
 'bye'
